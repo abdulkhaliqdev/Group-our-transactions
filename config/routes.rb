72bd6af792251f64c5834571e626a3a9bb83d826
abdulkhaliqdev <abdulkhaliql1f18bscs0159@gmail.com>
2021-04-11 02:43:44 +0500

    Done with migration of User and Group

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'groups#home'
+  get '/signup', to: 'users#new'
+  resources :users, only: [:show, :create]
+  resources :groups, only: [:index, :show, :new, :create]
+
+  get    '/login' => 'sessions#new'
+  post   '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end
