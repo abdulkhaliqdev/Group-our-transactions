bdc44302c962a4abf1f7dc8d48f78f51e0ea8587
abdulkhaliqdev <abdulkhaliql1f18bscs0159@gmail.com>
2021-04-11 19:58:18 +0500

    Add validation for Models

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,5 @@ class Group < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, uniqueness: true, length: { in: 4..30 }
 
-  has_many :transactions
+  has_many :entities
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :groups
   validates :name, uniqueness: true, presence: true, length: { in: 4..30 }
+
+  has_many :entities
 end
