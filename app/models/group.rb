d744bfd28c1bec2c9a607a841bc915b2fd1c1ca3
abdulkhaliqdev <abdulkhaliql1f18bscs0159@gmail.com>
2021-04-17 19:37:50 +0500

    Remove rubocop errors

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,4 @@ class Group < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, uniqueness: true
   has_many :transactions, class_name: 'Transaction', foreign_key: 'transaction_id'
-
 end
