e695909bca1c1e67ef074c117814260df5181cb9
abdulkhaliqdev <abdulkhaliql1f18bscs0159@gmail.com>
2021-04-20 21:32:12 +0500

    Fix group_id in transaction Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_150230) do
+ActiveRecord::Schema.define(version: 2021_04_20_163020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_04_20_150230) do
   create_table "transactions", force: :cascade do |t|
     t.string "name"
     t.float "amount"
-    t.bigint "group_id", null: false
+    t.bigint "group_id"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
